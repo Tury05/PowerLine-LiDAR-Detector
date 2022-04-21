28eb4ed5e57b4fa711e116c569b273db0d34cd48
Tury05 <arturo.ramos.rey@udc.es>
2022-04-21 14:01:38 +0200

    Sin avances pero con ideas nuevas

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 # Cosas por hacer
 - Comparar distintas funciones de clasificacion de suelo
 - Medir tiempos de ejecución
+
+# Ideas
+Pasar nube 3D a 2D y ejecutar ransac para detectar lineas 
+(Lineas electricas vistas desde arriba)
+Los postes serian perpendiculares a estas lineas
+Despues pensar en coo analizar eso
--- filtering.py
+++ filtering.py
@@ -9,18 +9,37 @@ json = """
     },
     {
         "type":"writers.las",
-        "filename":"ground_smrf.las"
+        "filename":"no_ground_smrf.las",
+        "where":"Classification != 2"
     }
 ]"""
 
 import pdal
+import time
+from WBT.whitebox_tools import WhiteboxTools
 
 def main():
+    wbt = WhiteboxTools()
     ground = pdal.Pipeline(json)
-    count = ground.execute()             #Numero de puntos
-    arrays = ground.arrays               #Array de todos los puntos
-    metadata = ground.metadata           #Metadatos
-    log = ground.log                     #Log
+    
+    start_time = time.time()
+    _ = ground.execute()
+    end_time = time.time()
+    
+    print("IO_time: %s\n" % (end_time - start_time))
+
+    """wbt.lidar_segmentation("../no_ground_smrf.las", "../segmentated.las", 
+    radius=2.0, 
+    num_iter=50, 
+    num_samples=10, 
+    threshold=0.15, 
+    model_size=15, 
+    max_slope=80.0, 
+    norm_diff=10.0, 
+    maxzdiff=1.0, 
+    classes=False, 
+    ground=False
+    )"""
 
 if __name__ == '__main__':
     main()
