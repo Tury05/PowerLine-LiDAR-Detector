12b7e9bfbf6aa6acafa84c862649b9d5f2869aa9
carvare <37734288+carvare@users.noreply.github.com>
2022-07-29 00:41:59 +0200

    Mejora script filtering.py
    Ahora descarta los subdirectorios y aquellos ficheros que no acaban en ".laz"

--- filtering.py
+++ filtering.py
@@ -90,18 +90,25 @@ def main():
         if not os.path.exists(os.path.join(path, "ground_truth")):
             os.makedirs(os.path.join(path, "ground_truth"))
     if isdir(path):
-        list_of_files = listdirs(path)
-        for file in list_of_files:
-            filename = os.path.basename(file)
-            ng_string = NO_GROUND[:59] + file + NO_GROUND[59:-49] + sys.argv[2] + "/no_grounds/NG_" + filename + NO_GROUND[-49:]
-            g_string = GROUND[:59] + file + GROUND[59:-10] + sys.argv[2] + "/grounds/G_" + filename + GROUND[-10:]
-            gt_string = GROUND_TRUTH[:59] + file + GROUND_TRUTH[59:-50] + sys.argv[2] + "/ground_truth/GROUND_TRUTH_" + filename + GROUND_TRUTH[-50:]
+        for mypath, mydirs, list_of_files in os.walk(path):
+            for file in list_of_files:   
+                if not file.endswith(".laz"):
+                    continue      
+                filename = os.path.basename(file)
+                filename = os.path.basename(os.path.join(os.path.basename(mypath), file))
+                print("tile:", filename)
+                ng_string = NO_GROUND[:59] + sys.argv[2] + file + NO_GROUND[59:-49] + sys.argv[2] + "/no_grounds/NG_" + filename + NO_GROUND[-49:]
+                g_string = GROUND[:59] + sys.argv[2] + file + GROUND[59:-10] + sys.argv[2] + "/grounds/G_" + filename + GROUND[-10:]
+                gt_string = GROUND_TRUTH[:59] + sys.argv[2] + file + GROUND_TRUTH[59:-50] + sys.argv[2] + "/ground_truth/GROUND_TRUTH_" + filename + GROUND_TRUTH[-50:]
 
-            if operation == "-g":
-                ground = pdal.Pipeline(g_string)
-            elif operation == "-gt":
-                ground = pdal.Pipeline(gt_string)
-            _ = ground.execute()
+                if operation == "-g":
+                    ground = pdal.Pipeline(g_string)
+                elif operation == "-gt":
+                    ground = pdal.Pipeline(gt_string)
+                elif operation == "-ng":
+                    # print("JSON", ng_string)
+                    ground = pdal.Pipeline(ng_string)
+                _ = ground.execute()
 
     elif isfile(path):
         filename = os.path.basename(path)
@@ -110,9 +117,12 @@ def main():
         gt_string = GROUND_TRUTH[:59] + path + GROUND_TRUTH[59:-50] + os.path.dirname(path) + "/ground_truth/GROUND_TRUTH_" + filename + GROUND_TRUTH[-50:]
 
         if operation == "-g":
-                ground = pdal.Pipeline(g_string)
+            ground = pdal.Pipeline(g_string)
         elif operation == "-gt":
             ground = pdal.Pipeline(gt_string)
+        elif operation == "-ng":
+            # print("JSON", ng_string)
+            ground = pdal.Pipeline(ng_string)
         _ = ground.execute()
 
     
